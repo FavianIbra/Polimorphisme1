30c6cdf8c74d1b8503edf83afc0e0ec70f4c3c7b
Favian Ibra <favian_yanuarta_30rpl@student.smktelkom-mlg.sch.id>
2022-03-09 12:11:08 +0700

    Polimorphisme1

---
+++ Vehicle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorphisme1;
+
+/**
+ *
+ * @author ACER
+ */
+public abstract class Vehicle {
+public void function(){
+    System.out.println("Tools transportation");
+            
+}
+public void fuel(){
+    System.out.println("fuelx");
+}
+public abstract void walk();
+   
+        
+    
+    
+}
